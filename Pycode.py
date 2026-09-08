6ba12ce8224362eaa580f5fcd33ee55d45a87e38
VanshikaKh <vanshikakhurana20@gmail.com>
2026-09-08 23:00:32 +0530

    input from user

--- Pycode.py
+++ Pycode.py
@@ -1,1 +1,16 @@
+# Ask the user to enter the first number
+num1 = float(input("Enter the first number: "))
+
+# Ask the user to enter the second number
+num2 = float(input("Enter the second number: "))
+
+# Display the available operations
+print("\nChoose an operation:")
+print("+  Addition")
+print("-  Subtraction")
+print("*  Multiplication")
+print("/  Division")
+
+# Ask the user to choose an operation
+operation = input("Enter the operation (+, -, *, /): ")
 
